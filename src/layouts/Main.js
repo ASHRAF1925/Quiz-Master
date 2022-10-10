1f54ce6a82aa21b6d0930a608af3c338170a6466
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-11 00:05:39 +0600

    main layout done

---
+++ src/layouts/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header/Header';
+import Navbar from '../Components/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Main;
