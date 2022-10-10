ab4759af394a5a1dce0c910b3484956694da3b41
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-11 03:46:29 +0600

    question added

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
-
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Question from './Question/Question';
 const Questions = () => {
     const allquestions=useLoaderData();
-    console.log(allquestions.data);
+  
     const{total,id,name,logo,questions}=allquestions.data;
-    console.log(questions);
+    console.log(typeof(questions));
     return (
         <div>
-            <h1>Hello {name}</h1>
+            <h1>Welcome to the Quiz of  {name}</h1>
+            <Card.Img style={{ width: '18rem' }}variant="top" src={logo} className="bg-dark " />
+            <h4 className='mt-4'>There are total <b>{total} Questions</b></h4>
+
+            <Container>
+        <Row>
+          <Col lg={12} >
+            <Row>
+            { 
+                questions.map(question=><Question>
+                    key={question.id},
+                    Question={question}
+                </Question>)
+            }
+            </Row>
+           </Col>
+         
+        </Row>
+    
+      </Container>
         </div>
     );
 };
