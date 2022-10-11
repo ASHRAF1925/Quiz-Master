1d9c0ead84f5dd0f2c4679288032fd14600a975a
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-11 19:02:22 +0600

    toast done

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -4,11 +4,34 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import './Question.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Question = (questions) => {
-  console.log(questions.children[1]);
+  // console.log(questions.children[3].correctAnswer);
+
+  const checkanswer=(correct_answer,selected_answer)=>{
+
+    console.log(selected_answer);
+   
+              
+    if(selected_answer===correct_answer)
+    {
+      toast.success("Congratulations.You have Completed the Plan Successfully!",{position:"top-center"},{position:"top-center"});
+      
+
+    }
+    else{
+      toast.error("Worng Answer",{position:"top-center"},{position:"top-center"});
+      
+
+
+    }
+  }
 
   const question_text = Substring(questions.children[3].question);
+
   return (
     <div className="bg-light mt-5 p-5 container">
       <h1>
@@ -20,14 +43,18 @@ const Question = (questions) => {
           {questions.children[3].options.map((option) => (
             <Col lg={6} className="mt-4">
               <input type="radio" id={option} name={questions.children[1] + 1}value={option} />
-              <label for={option}><div className="mx-4 option-box">{option}</div></label>
-              <br></br>
+              
+              
+              <label for={option} onClick={()=>checkanswer(questions.children[3].correctAnswer,option)}><div className="mx-4 option-box">{option}</div></label>
+              <ToastContainer autoClose={1000} />
+              
 
               
             </Col>
           ))}
         </Row>
       </Container>
+   
     </div>
   );
 };
--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Question from '../Question/Question';
+import React from "react";
+import { useLoaderData } from "react-router";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Question from "../Question/Question";
 const Questions = () => {
-    const allquestions=useLoaderData();
-  
-    const{total,id,name,logo,questions}=allquestions.data;
-    // console.log(typeof(questions));
-    return (
-        <div>
-            <h1>Welcome to the Quiz of  {name}</h1>
-            <Card.Img style={{ width: '18rem' }}variant="top" src={logo} className="bg-dark " />
-            <h4 className='mt-4'>There are total <b>{total} Questions</b></h4>
+  const allquestions = useLoaderData();
 
-            <Container>
+  const { total, id, name, logo, questions } = allquestions.data;
+
+  return (
+    <div>
+      <h1>Welcome to the Quiz of {name}</h1>
+      <Card.Img
+        style={{ width: "18rem" }}
+        variant="top"
+        src={logo}
+        className="bg-dark "
+      />
+      <h4 className="mt-4">
+        There are total <b>{total} Questions</b>
+      </h4>
+
+      <Container>
         <Row>
-          <Col lg={12} >
+          <Col lg={12}>
             <Row>
-            { 
-                questions.map((question,index)=>
-                    // console.log(index);
-                
+              {questions.map((question, index) => (
+                // console.log(index);
+
                 <Question>
-                    key={index},
-                    Question={question},
-                    
-                </Question>)
-            }
+                  key={index}, Question={question},
+                </Question>
+              ))}
             </Row>
-           </Col>
-         
+          </Col>
         </Row>
-    
       </Container>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Questions;
